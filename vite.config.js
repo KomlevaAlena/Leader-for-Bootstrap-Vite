5984e9572a12d7d18c4d23e9dbfbcc23a5db1eec
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-03-16 04:18:58 +0500

    add path for svg

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
 
-export default {
-  root: resolve(__dirname, 'src'), // Корневая папка — src
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'), // Корневая папка — src
   build: {
-    outDir: resolve(__dirname, 'dist'), // Папка для сборки — dist
+    outDir: path.resolve(__dirname, 'dist'), // Папка для сборки — dist
   },
   server: {
     port: 8080, // Порт для dev-сервера
   },
-};
+  plugins: [
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(__dirname, 'src/images/icons')], // Путь к иконкам
+      symbolId: 'icon-[name]', // Шаблон для id иконок
+    }),
+  ],
+});
